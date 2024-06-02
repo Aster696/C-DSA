cc08bed32a2e5f007d183a63b07506a61c25962d
Aster696 <aster.r@codezen.in>
2024-06-02 15:43:10 +0530

    first

--- stack.cpp
+++ stack.cpp
@@ -73,7 +73,7 @@ public:
         if(isEmpty()) {
             cout << "Stack is empty" << endl;
         }else {
-            cout << "Total number of element in stack are: " + top + 1 << endl;
+            cout << "Total number of element in stack is: " << top + 1 << endl;
         }
     }
 
