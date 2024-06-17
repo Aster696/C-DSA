7afaffc298bc46a9f9e61686258d59d042a61710
ASTER <asterrebeiro69@gmail.com>
2024-06-17 22:01:55 +0530

    Fist commit

--- singlyLikedList.cpp
+++ singlyLikedList.cpp
@@ -65,7 +65,7 @@ public:
     }
 
     // prepend node - Attach node at the start
-    prependNode(Node *n) {
+    void prependNode(Node *n) {
         if(nodeKeyExists(n -> key) != NULL) {
             cout << "Node already exist with key value : " << n -> key << ". Append another node with different key value." << endl;
         }else {
@@ -83,6 +83,40 @@ public:
             ptr = ptr -> next;
         }
     }
+
+    // Insert node in between list after particular node
+    void insertNode(Node *n, int k) {
+        // Node *ptr = head;
+        // Node *temp = NULL;
+        // if(nodeKeyExists(k) == NULL) {
+        //     cout << "Key does not exist. Please try with different key" << endl; 
+        // }else if(nodeKeyExists(n -> key) != NULL) {
+        //     cout << "Node already exist with key value : " << n -> key << ". Append another node with different key value." << endl;
+        // }else {
+        //     while(ptr != NULL) {
+        //         if(ptr -> key == k) {
+        //             temp = ptr -> next;
+        //             ptr -> next = n;
+        //             n -> next = temp;
+        //             cout << "Node inserted successfull" << endl;
+        //         }
+        //         ptr = ptr -> next;
+        //     }
+        // }
+
+        // another way of doing it
+        Node *ptr = nodeKeyExists(k);
+        if(ptr == NULL) {
+            cout << "Key does not exist. Please try with different key" << endl; 
+        }else if(nodeKeyExists(n -> key) != NULL) {
+            cout << "Node already exist with key value : " << n -> key << ". Append another node with different key value." << endl;
+        }else {
+            n -> next = ptr -> next;
+            ptr -> next = n;
+            cout << "Node inserted successfull" << endl;
+        }
+    }
+
 };
 
 int main() {
@@ -90,6 +124,7 @@ int main() {
     Node *n2 = new Node(2, 66);
     Node *n3 = new Node(3, 99);
     Node *n0 = new Node(69, 44);
+    Node *insertNode = new Node(84, 77);
 
     SiglyLinkedList si;
     si.appendNode(n1);
@@ -97,6 +132,8 @@ int main() {
     si.appendNode(n3);
     si.displayNodeList();
     si.prependNode(n0);
+    si.displayNodeList();
+    si.insertNode(insertNode, 45);
     si.displayNodeList();
     return 0;
 }
