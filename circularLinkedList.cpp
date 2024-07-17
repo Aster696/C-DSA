32427dbf7a381f9f93fcb340e0d1d89f54087c75
Aster696 <aster.r@codezen.in>
2024-07-17 06:00:59 +0530

    fist commit

--- circularLinkedList.cpp
+++ circularLinkedList.cpp
@@ -1,3 +1,7 @@
+#include <iostream>
+
+using namespace std;
+
 class Node{
     public:
     int key, data;
@@ -29,14 +33,31 @@ class CircularLinkedList {
 
     // check if node is null 
     Node *nodeExist(int k) {
+        Node temp = NULL;
         Node *ptr = head;
         while(ptr !== NULL) {
             if(prt -> key == k) {
+                temp = ptr;
                 break;
             }else {
                 ptr = ptr -> next;
             }
         }
-        return ptr;
+        delete ptr;
+        return temp;
+    }
+
+    // append node
+    void appendNode(Node *node) {
+        Node *ptr = head;
+        if(nodeExist(node -> key) != NULL) {
+            cout << "Node key already exist" << endl;
+        }else {
+            while(ptr != NULL) {
+                ptr = ptr -> next;
+            }
+            ptr -> next = node;
+            cout << "Node appended successfully" << endl;
+        }
     }
 };
